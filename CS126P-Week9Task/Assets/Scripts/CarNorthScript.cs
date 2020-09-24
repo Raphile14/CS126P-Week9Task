84e512c8527563ba6f920bf0ede8f4c5098b1dfb
Raphile14 <rllddalangin@gmail.com>
2020-09-24 15:32:04 +0800

    indev 1.0.0 (09/24/2020)

---
+++ CS126P-Week9Task/Assets/Scripts/CarNorthScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarNorthScript : MonoBehaviour
+{
+    public float speed = 1;
+    float z;
+    float y;
+
+    void Start()
+    {
+        y = transform.position.y;
+        z = transform.position.z;
+    }
+
+    void Update()
+    {
+        if (transform.position.x >= 5)
+        {
+            transform.position = new Vector3(-5, y, z);
+        }
+        transform.position = new Vector3(transform.position.x + speed/100 * Time.deltaTime, y, z);
+    }
+}
---
+++ CS126P-Week9Task/Assets/Scripts/SunScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunScript : MonoBehaviour
+{
+
+    public float speed;
+    public float limit;
+
+    void Start()
+    {
+        transform.position = new Vector3(-limit, 5, 0);
+    }
+
+    void Update()
+    {
+        if (transform.position.x >= limit)
+        {
+            transform.position = new Vector3 (-limit, 5, 0);
+        }
+        transform.position = new Vector3 (transform.position.x + speed * Time.deltaTime, 5, 0);
+    }
+}
